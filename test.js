3536feac4e7b1f3c4a9ea6d8f0ccf575550ca660
Your Name~ <you@example.com>
2025-01-05 21:52:35 +0200

    Your commit message

--- test.js
+++ test.js
@@ -11,3 +11,7 @@ test('adds -1 + 1 to equal 0', () => {
 test('adds 0 + 0 to equal 0', () => {
   expect(add(0, 0)).toBe(0);
 });
+
+test('adds 0 + 0 to equal 0', () => {
+  expect(add(0, 3)).toBe(5);
+});
